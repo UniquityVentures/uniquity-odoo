0cc88b9e727dddcbf976b68a394bea910c12b6fb
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-09-17 22:41:29 +0530

    Changed models according to new spec

--- gandola_manager/models/models.py
+++ gandola_manager/models/models.py
@@ -8,8 +8,14 @@ class Gandola(models.Model):
     _description = 'Gandola Management'
 
     name = fields.Char(string='Gandola Name', required=True)
-    current_site = fields.Many2one('gandola_manager.site', string='Current Site')
-    sites = fields.Many2many('gandola_manager.site', string='Associated Sites')
+
+
+class LendingRecord(models.Model):
+    _name = 'gandola_manager.lending_record'
+    gandola = fields.One2many('gandola_manager.gandola', 'lending_records')
+    site = fields.One2many('gandola_manager.site', 'lending_records')
+    start_date = fields.Date(string='Start Date')
+    end_date = fields.Date(string='End Date')
 
 
 class Site(models.Model):
@@ -18,7 +24,8 @@ class Site(models.Model):
 
     name = fields.Char(string='Site Name', required=True)
     address = fields.Text(string='Address')
-    gandolas = fields.One2many('gandola_manager.gandola', 'current_site', string='Gandolas')
+
+class Invoice(models.Model):
+    site = fields.One2many('gandola_manager.site', 'invoices')
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
-
