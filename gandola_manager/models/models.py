5b9ced6f74ab48b17157fb8abb225c1aab221e66
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-09-17 23:47:19 +0530

    View and create of all models for gandola

--- gandola_manager/models/models.py
+++ gandola_manager/models/models.py
@@ -8,11 +8,13 @@ class Gandola(models.Model):
     _description = 'Gandola Management'
 
     name = fields.Char(string='Gandola Name', required=True)
-    lending_records = fields.Many2one('gandola_manager.lending_record', "gandola")
+    lending_records = fields.Many2one('gandola_manager.lending_record')
 
 
 class LendingRecord(models.Model):
     _name = 'gandola_manager.lending_record'
+    _description = 'Lending Records'
+
     gandola = fields.One2many('gandola_manager.gandola', 'lending_records')
     site = fields.One2many('gandola_manager.site', 'lending_records')
     start_date = fields.Date(string='Start Date')
@@ -25,11 +27,13 @@ class Site(models.Model):
 
     name = fields.Char(string='Site Name', required=True)
     address = fields.Text(string='Address')
-    lending_records = fields.Many2one('gandola_manager.lending_record', "site")
-    invoices = fields.Many2one('gandola_manager.invocie', "site")
+    lending_records = fields.Many2one('gandola_manager.lending_record')
+    invoices = fields.Many2one('gandola_manager.invoice')
 
 class Invoice(models.Model):
     _name = 'gandola_manager.invoice'
+    _description = 'Invoice Management'
+
     site = fields.One2many('gandola_manager.site', 'invoices')
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
