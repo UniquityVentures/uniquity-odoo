056b583c279b062234b282424d644449d203fc10
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-09-17 23:52:53 +0530

    Added dependency on invoice

--- gandola_manager/__manifest__.py
+++ gandola_manager/__manifest__.py
@@ -1,31 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Gandola",
-
-    'description': """
+    "name": "Gandola",
+    "description": """
 Gandola Manager App for Uniquity Ventures
 =========================================
 
     """,
-
-    'author': "Uniquity Ventures",
-    'category': 'Administration',
-    'version': '0.1',
-
+    "author": "Uniquity Ventures",
+    "category": "Inventory",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base", "account"],
     "auto_install": True,
     "license": "LGPL-3",
     # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/views.xml",
+        "views/templates.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
-
