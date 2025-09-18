b0224069a09610a5bb40e9e2def74f3a037f5716
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-09-18 11:48:42 +0530

    Only Site and Gandola

--- gandola_manager/models/models.py
+++ gandola_manager/models/models.py
@@ -2,23 +2,11 @@
 
 from odoo import models, fields, api
 
-
 class Gandola(models.Model):
     _name = 'gandola_manager.gandola'
     _description = 'Gandola Management'
 
     name = fields.Char(string='Gandola Name', required=True)
-    lending_records = fields.Many2one('gandola_manager.lending_record')
-
-
-class LendingRecord(models.Model):
-    _name = 'gandola_manager.lending_record'
-    _description = 'Lending Records'
-
-    gandola = fields.One2many('gandola_manager.gandola', 'lending_records')
-    site = fields.One2many('gandola_manager.site', 'lending_records')
-    start_date = fields.Date(string='Start Date')
-    end_date = fields.Date(string='End Date')
 
 
 class Site(models.Model):
@@ -29,11 +17,7 @@ class Site(models.Model):
     address = fields.Text(string='Address')
     lending_records = fields.Many2one('gandola_manager.lending_record')
     invoices = fields.Many2one('gandola_manager.invoice')
-
-class Invoice(models.Model):
-    _name = 'gandola_manager.invoice'
-    _description = 'Invoice Management'
-
-    site = fields.One2many('gandola_manager.site', 'invoices')
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
+    gandola = fields.Many2many('gandola_manager.gandola')
+
