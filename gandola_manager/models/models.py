23f7b8038ce5ad64b165f4595db4f254ca713041
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-09-18 11:51:35 +0530

    lending_record and invoice delete

--- gandola_manager/models/models.py
+++ gandola_manager/models/models.py
@@ -15,8 +15,6 @@ class Site(models.Model):
 
     name = fields.Char(string='Site Name', required=True)
     address = fields.Text(string='Address')
-    lending_records = fields.Many2one('gandola_manager.lending_record')
-    invoices = fields.Many2one('gandola_manager.invoice')
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
     gandola = fields.Many2many('gandola_manager.gandola')
